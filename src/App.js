006649fa7826d502e71a367bdbe8cdbdf161f243
SeungHanLee <swq3064@naver.com>
2024-01-15 16:36:12 +0900

    #초기 설정

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:8888/api/example')
+        .then(response =>
+            setData(response.data)
+        )
+        .catch(error => console.error(error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <h1>안녕{data}</h1>
+      </div>
   );
 }
 
